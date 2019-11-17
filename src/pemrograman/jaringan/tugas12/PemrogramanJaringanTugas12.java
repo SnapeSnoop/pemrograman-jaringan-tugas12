b3ac77ab0e349e707b517ceda00ac14e533b4517
SnapeSnoop <ekamahendrabagaskara@gmail.com>
2019-11-17 23:05:50 +0700

    Tugas12

--- src/pemrograman/jaringan/tugas12/PemrogramanJaringanTugas12.java
+++ src/pemrograman/jaringan/tugas12/PemrogramanJaringanTugas12.java
@@ -11,17 +11,70 @@
  */
 package pemrograman.jaringan.tugas12;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /**
  *
  * @author od3ng
  */
-public class PemrogramanJaringanTugas12 {
+public class PemrogramanJaringanTugas12 implements Runnable {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private GreetClient client;
+    private Port portView;
+    private ChatGui view;
+    private String mess = "";
+
+    public PemrogramanJaringanTugas12() {
+        this.client = new GreetClient();
+        this.portView = new Port();
+        this.view = new ChatGui();
+        this.portView.setTitle("Port Input");
+        this.portView.setVisible(true);
+
+        this.portView.getBtnOk().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (String.valueOf(portView.getPortText().getText()).equals("6666")) {
+                    client.startConnection("127.0.0.1",
+                            Integer.valueOf(portView.getPortText().getText()));
+                    portView.setVisible(false);
+                    view.setTitle("Client Chat");
+                    view.setVisible(true);
+                } else {
+                    portView.getPortText().setText("");
+                }
+            }
+        });
+        this.view.getBtnKirim().addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                mess += time() + "-> " + String.valueOf(view.getTextField().getText() + "\n");
+                String response = client.sendMessage(time() + "<-" + String.valueOf(view.getTextField().getText()));
+                mess += response + "\n";
+            }
+        });
     }
-    
+
+    public String time() {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("HH:mm:ss.SSS");
+        String time = simpleDateFormat.format(date);
+        return time;
+    }
+
+    @Override
+    public void run() {
+        do {
+            if (this.view.getTxtArea().getText().equals(mess) == false) {
+                this.view.getTxtArea().setText(mess);
+            }
+        } while (true);
+    }
+
+    public static void main(String[] args) {
+        new Thread(new PemrogramanJaringanTugas12()).start();
+    }
 }
